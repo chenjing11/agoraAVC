e1dffd60608dbbc79e335648fd999e179d732330
chenjing <chenjing@agora.io>
2019-09-16 15:57:28 +0800

    first init

---
+++ avc_fulltest/test_functionality.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 -*-
+from airtest.core.api import *
+init_device("Android")
+connect_device("Android:///")
+import airtest.core.android.android
+from PIL import Image
+import cv2
+import pytest
+from common import case_tag
+from common.maincase import Android_AVC
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r'/usr/local/Cellar/tesseract/4.0.0.1/bin/tesseract'
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco=AndroidUiautomationPoco()
+
+class TestAndroid:
+
+    def setup(self):
+        self.avc = Android_AVC()
+        self.channel_name = "AVCAUTO"
+        self.password = "avctest"
+        self.packageName = "io.agora.videocall"
+
+    def tearDown(self):
+        pass
+
+    @pytest.mark.tags(case_tag.ANDROID, case_tag.MEDIUM, case_tag.AUTOMATED, case_tag.FUNCTIONALITY)
+    def test_joinChannel(self):
+        avc = self.avc
+        avc.setCurrentDevice(0)
+        avc.startAVC(self.packageName)
+        avc.inputChannelName(self.channel_name)
+        avc.inputPassword(self.password)
+        avc.joinChannel()
---
+++ common/maincase.py
@@ -0,0 +1,33 @@
+#-*- coding:utf-8 -*-
+from airtest.core.api import *
+from PIL import Image
+import cv2
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd=r'/usr/local/Cellar/tesseract/4.0.0_1/bin/tesseract'
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco=AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+auto_setup(__file__)
+
+class Android_AVC():
+    def __init__(self):
+        self.interval = 2
+
+    def setCurrentDevice(self,device):
+        set_current(device)
+
+    def startAVC(self,packageName):
+        start_app(packageName)
+        wait(Template(r"tpl1568208771836.png", record_pos=(-0.002, -0.009), resolution=(1080, 1920)))
+        sleep(self.interval)
+
+    def inputChannelName(self,channelname):
+        poco("io.agora.vcall:id/edit_name").click()
+        text(channelname)
+
+    def inputPassword(self,password):
+        poco("io.agora.vcall:id/edit_password").click()
+        text(password)
+
+    def joinChannel(self):
+        poco("io.agora.vcall:id/btn_join").click()
+
